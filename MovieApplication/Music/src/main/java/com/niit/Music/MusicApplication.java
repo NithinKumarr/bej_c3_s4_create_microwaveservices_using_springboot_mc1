87870c9a0d9a2f87db18bce412bcdb813affc251
girijaugge123 <girijaugge123@gmail.com>
2023-01-11 08:50:00 +0530

    solution added

---
+++ MovieApplication/Music/src/main/java/com/niit/Music/MusicApplication.java
@@ -0,0 +1,25 @@
+package com.niit.Music;
+
+import com.niit.Music.filter.JwtFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MusicApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MusicApplication.class, args);
+
+	}
+	@Bean
+	public FilterRegistrationBean jwtFilter(){
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean<>();
+		filterRegistrationBean.setFilter(new JwtFilter());
+		filterRegistrationBean.addUrlPatterns("/api/v1/*");
+		return filterRegistrationBean;
+	}
+
+}
